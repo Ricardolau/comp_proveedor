c40685c89c5df100da2c681eb40460cb4eb1842e
solucion40 <ricardolau6@gmail.com>
2015-10-22 00:54:43 +0200

    Modifico script para que meta parametros dentro de la tabla _extenxsions en el campo de params del registro del componente, queda pendiente eleliminar paradas de control

--- script.php
+++ script.php
@@ -15,66 +15,60 @@ class com_proveedorInstallerScript {
 		// Carga el fichero config de proveedor
         $files = JFolder::files( $path_componente , "config.xml" , false ,true); 
 		
-		$params = &JComponentHelper::getParams('com_proveedor');
-		//~ $params =JComponentHelper::getParams('com_proveedor');
-        echo ' ******************************************************************************';
-        echo JPATH_ROOT;
-        echo ' ******************************************************************************';
-        
-        echo '<pre>';
-        print_r($files);
-        echo '</pre>';
-        echo '<pre>';
-        echo ' ******************************************************************************';
-        print_r($params);
-        echo '</pre>';
-		//~ $donwloader = new FOFDownload();
-		//~ $xmlSource = $donwloader->getFromURL($files[0]);
-        $formxml = simplexml_load_file($files[0]);
-		$rootAttributes = $formxml->children();
 		
-		//~ $xml1 = JForm::getInstance('config', $files[0]);
-		      //~ 
-        //~ $campos = $xml1->getFieldset(); 
-		//~ foreach ($fieldsets as $fieldset) {
-        //~ echo '<h1>' . $fieldset->name . '</h1>';
-        //~ $fields = $form11->getFieldset($fieldset->name);
-        //~ foreach ($fields as $field) {
-            //~ echo $field->label;
-            //~ echo $field->input;
-        //~ }
-		//~ }
+        echo ' ********************JPATH ROOT *********************************************************';
         echo '<pre>';
-        echo ' ********************* XML *********************************************************';
-        print_r($rootAttributes);
-        //~ print_r($nuevoxml);
+        echo JPATH_ROOT; // Es la ruta de la instalación... 
+        echo $path_componente; // Es la ruta de componente... 
         echo '</pre>';
-       
-       
-       
-       
-       
-       
-        
-        //~ $params['version'] = $this->com_proveedor;
-        //~ $this->setParams( $params );
-//~ 
-        //~ $db    = JFactory::getDBO();
-        //~ $query = $db->getQuery(true);
-        //~ $query
-            //~ ->update('#__update_sites')
-            //~ ->set("`enabled`='1'")
-            //~ ->where("`name`='ImproveMyCity'");
-        //~ $db->setQuery($query);
-        //~ $db->execute();
-        //~ 
-        return true;        
+        echo ' *******************************  FILES  ***********************************************';
+        echo '<pre>';
+        print_r($files); // $files es la ruta del fichero config.xml
+        echo '</pre>';
+        $formxml = simplexml_load_file($files[0]); 
+		$rootAttributes = $formxml->children(); // Genera objecto SimpleXMLElement , contiene parametros por defecto
+		/** Creamos un bucle para recorra todos los grupos de parametros del fichero */
+		foreach($rootAttributes->fieldset as $fieldset)
+		{
+			/* Necesito identificar si el grupo parametros es de permisos, ya que estos no se ponen en parametros*/
+			$Nombregrupo =$fieldset->attributes(); // Obtenemos el nombre grupo de parametro si tienes.. 
+			echo 'Nombre de grupo permisos:'.$Nombregrupo[name].'<br>';
+			echo ' ********************* XML *********************************************************<br>';
+			echo '<pre>';
+			print_r($fieldset); // $files es la ruta del fichero config.xml
+			echo '</pre>';
+			echo ' ********************* For Set Params *********************************************************<br>';
+			
+			/* Si el Nombregrupo es igual permissions no lo metemos como parametro */
+			if ($Nombregrupo <>'permissions') {
+				echo '<pre>';
+				$parametros = array(); // Creo array donde voy añadir parametros y valor defecto
+				foreach($fieldset->field as $child)
+				{
+				echo '******** child ************<br>';
+				$atributos=$child->attributes();
+				$name= $atributos[name];
+				$default = $atributos['default'];
+				$parametros[strval($name)]=strval($default) ;
+				echo $name.':'.$default.'<br>';
+				echo '******** fin child ************<br>';
+				}
+				echo '</pre>';
+				$this->setParams($parametros);// Enviamos el array con los parametros a funcion setParams
+			}
+		} 
+		return true;        
     }
     
     /*
      * establece valores de los parámetros en la fila del componente de la tabla de extensión
      */
     public function setParams($param_array) {
+		echo ' ******* Estes el array parametros que añadimos a tabla extensions **********<br>';
+        echo '<pre>';
+        print_r($param_array);
+        echo '</pre>';
+              
         if ( count($param_array) > 0 ) {
             // read the existing component value(s)
             $db = JFactory::getDbo();
@@ -89,9 +83,10 @@ class com_proveedorInstallerScript {
             
             $db->setQuery('UPDATE #__extensions SET params = ' .
                 $db->quote( $paramsString ) .
-                ' WHERE name = "com_proveedor' );
+                ' WHERE name = "com_proveedor"' );
                 $db->query();
         }
     }
+    
 
 }
