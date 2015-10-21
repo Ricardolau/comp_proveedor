5d5089c18ff47cf30b029f75b7af01758e8ce4bd
solucion40 <ricardolau6@gmail.com>
2015-10-22 01:01:53 +0200

    Elimino los puntos control script.php , ya que mete perfectamente los parametros del componente en la tabla extensions...

--- script.php
+++ script.php
@@ -7,53 +7,32 @@
 class com_proveedorInstallerScript {
 	/*
      * Todo lo que aquí sucede después de la instalación / actualización / desinstalación 
+     * Si ponemos controles print_r no los muestra en despues de la instalación... en la misma
+     * pantalla instalar.
      */
     public function postflight( $type, $parent ) {
-        // always create or update version parameter
-       //~ jimport('joomla.application.component.controller');
-		$path_componente = JPATH_ROOT.'/administrator/components/com_proveedor';
+       	$path_componente = JPATH_ROOT.'/administrator/components/com_proveedor';
 		// Carga el fichero config de proveedor
         $files = JFolder::files( $path_componente , "config.xml" , false ,true); 
-		
-		
-        echo ' ********************JPATH ROOT *********************************************************';
-        echo '<pre>';
-        echo JPATH_ROOT; // Es la ruta de la instalación... 
-        echo $path_componente; // Es la ruta de componente... 
-        echo '</pre>';
-        echo ' *******************************  FILES  ***********************************************';
-        echo '<pre>';
-        print_r($files); // $files es la ruta del fichero config.xml
-        echo '</pre>';
-        $formxml = simplexml_load_file($files[0]); 
+		 $formxml = simplexml_load_file($files[0]); 
 		$rootAttributes = $formxml->children(); // Genera objecto SimpleXMLElement , contiene parametros por defecto
 		/** Creamos un bucle para recorra todos los grupos de parametros del fichero */
 		foreach($rootAttributes->fieldset as $fieldset)
 		{
 			/* Necesito identificar si el grupo parametros es de permisos, ya que estos no se ponen en parametros*/
 			$Nombregrupo =$fieldset->attributes(); // Obtenemos el nombre grupo de parametro si tienes.. 
-			echo 'Nombre de grupo permisos:'.$Nombregrupo[name].'<br>';
-			echo ' ********************* XML *********************************************************<br>';
-			echo '<pre>';
-			print_r($fieldset); // $files es la ruta del fichero config.xml
-			echo '</pre>';
-			echo ' ********************* For Set Params *********************************************************<br>';
-			
 			/* Si el Nombregrupo es igual permissions no lo metemos como parametro */
-			if ($Nombregrupo <>'permissions') {
-				echo '<pre>';
+			if ($Nombregrupo[name] <>'permissions') {
 				$parametros = array(); // Creo array donde voy añadir parametros y valor defecto
 				foreach($fieldset->field as $child)
 				{
-				echo '******** child ************<br>';
 				$atributos=$child->attributes();
 				$name= $atributos[name];
 				$default = $atributos['default'];
+				// Añadimos nombre parametro y valor por defecto, strval nos ayuda meter solo valor y no array ,, SimpleXMl Object
 				$parametros[strval($name)]=strval($default) ;
-				echo $name.':'.$default.'<br>';
-				echo '******** fin child ************<br>';
 				}
-				echo '</pre>';
+				
 				$this->setParams($parametros);// Enviamos el array con los parametros a funcion setParams
 			}
 		} 
@@ -64,11 +43,7 @@ class com_proveedorInstallerScript {
      * establece valores de los parámetros en la fila del componente de la tabla de extensión
      */
     public function setParams($param_array) {
-		echo ' ******* Estes el array parametros que añadimos a tabla extensions **********<br>';
-        echo '<pre>';
-        print_r($param_array);
-        echo '</pre>';
-              
+		  
         if ( count($param_array) > 0 ) {
             // read the existing component value(s)
             $db = JFactory::getDbo();
